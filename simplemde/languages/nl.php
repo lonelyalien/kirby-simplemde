d09d632ca0d528b4b232f0a1d3f826cd63d71209
Jimi Robaer <jimi@lonelyalien.com>
2018-01-12 14:54:39 +0100

    Add extra language fields

---
+++ simplemde/languages/nl.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+  // Toolbar buttons
+  'button.h1'              => 'Eerste hoofding',
+  'button.h2'              => 'Tweede hoofding',
+  'button.h3'              => 'Derde hoofding',
+  'button.bold'            => 'Vetgedrukt',
+  'button.italic'          => 'Schuingedrukt',
+  'button.unordered-list'  => 'Lijst',
+  'button.ordered-list'    => 'Genummerde lijst',
+  'button.quote'           => 'Quote',
+  'button.horizontal-rule' => 'Horizontale scheidingslijn',
+  'button.link'            => 'Link',
+  'button.page'            => 'Pagina',
+  'button.file'            => 'Bestand',
+  'button.email'           => 'E-mail',
+  'button.tel'             => 'Telefoon',
+
+  // Pagelink
+  'pagelink.placeholder'   => 'Zoek naar pagina',
+  'pagelink.no-results'    => 'Geen resultaten',
+
+  // Filelink
+  'filelink.placeholder'   => 'Zoek naar bestand',
+
+];
